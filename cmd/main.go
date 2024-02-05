dc8b7eafa971123e566638242b1806d356d727b9
MRskyPG <mrogalsky8113@gmail.com>
2024-02-05 13:35:27 +0600

    Add working BMI

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"body-calculator-tg-bot/pkg/bodycalc"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"strconv"
+)
+
+type UserState int
+
+const (
+	InitialState UserState = iota
+	WeightState
+	HeightState
+)
+
+const (
+	minWeight = 3.0
+	maxWeight = 450.0
+	minHeight = 40.0
+	maxHeight = 225.0
 )
 
 var (
 	bot          *tgbotapi.BotAPI
+	bmi          = bodycalc.NewBMI(0.0, 0.0)
+	userStates   map[int64]UserState
 	startMessage = "Привет! Данный калькулятор позволит рассчитать ИМТ, количество калорий, которое необходимо " +
 		"вашему организму в зависимости от вашего роста, веса, возраста и степени физической активности. Для начала перейдите в меню:"
 	goToMenu = tgbotapi.NewInlineKeyboardMarkup(tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData("Меню", "menu")))
@@ -41,15 +60,15 @@ func main() {
 
 	log.Println("Bot was started.")
 
+	userStates = make(map[int64]UserState) // Хранение состояний пользователей
+
 	for update := range updates {
 		if update.CallbackQuery != nil {
 			callbacks(update)
 		} else if update.Message.IsCommand() {
 			commands(update)
-		} else {
-			//messages
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Неизвестная команда. Вернуться в меню: /menu")
-			sendMessage(msg)
+		} else if update.Message != nil {
+			chooseState(userStates, update)
 		}
 	}
 
@@ -60,9 +79,18 @@ func callbacks(update tgbotapi.Update) {
 	chatId := update.CallbackQuery.Message.Chat.ID
 	switch cb {
 	case "menu":
+		userStates[chatId] = InitialState
 		msg := tgbotapi.NewMessage(chatId, "Выберите необходимую опцию:")
 		msg.ReplyMarkup = menu
 		sendMessage(msg)
+	case "bmi":
+		chatID := update.CallbackQuery.Message.Chat.ID
+
+		msg := tgbotapi.NewMessage(chatID, "Введите ваш вес:")
+		bot.Send(msg)
+
+		userStates[chatID] = WeightState
+
 	}
 }
 
@@ -71,10 +99,12 @@ func commands(update tgbotapi.Update) {
 	chatId := update.Message.Chat.ID
 	switch command {
 	case "start":
+		userStates[chatId] = InitialState
 		msg := tgbotapi.NewMessage(chatId, startMessage)
 		msg.ReplyMarkup = goToMenu
 		sendMessage(msg)
 	case "menu":
+		userStates[chatId] = InitialState
 		msg := tgbotapi.NewMessage(chatId, "Выберите необходимую опцию:")
 		msg.ReplyMarkup = menu
 		sendMessage(msg)
@@ -89,4 +119,57 @@ func sendMessage(msg tgbotapi.Chattable) {
 		log.Panicf("Failed to send message: %v", err)
 	}
 	log.Println("Message was sended.")
+}
+
+func chooseState(userStates map[int64]UserState, update tgbotapi.Update) {
+	chatId := update.Message.Chat.ID
+	switch userStates[chatId] {
+	case InitialState:
+		bmi = bodycalc.NewBMI(0.0, 0.0)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Неизвестная команда. Вернуться в меню: /menu")
+		sendMessage(msg)
+	case WeightState:
+		weight, err := strconv.ParseFloat(update.Message.Text, 64)
+		if err != nil {
+			msg := tgbotapi.NewMessage(chatId, "Ошибка! Введите число для веса.")
+			sendMessage(msg)
+			return
+		}
+		if weight < minWeight || weight > maxWeight {
+			msg := tgbotapi.NewMessage(chatId, "Ошибка! Введите корректное значение веса")
+			sendMessage(msg)
+			return
+		}
+
+		bmi.Weight = weight
+
+		msg := tgbotapi.NewMessage(chatId, "Введите ваш рост:")
+		bot.Send(msg)
+
+		userStates[chatId] = HeightState
+
+	case HeightState:
+		height, err := strconv.ParseFloat(update.Message.Text, 64)
+		if err != nil {
+			msg := tgbotapi.NewMessage(chatId, "Ошибка! Введите число для роста.")
+			sendMessage(msg)
+			return
+		}
+		if height < minHeight || height > maxHeight {
+			msg := tgbotapi.NewMessage(chatId, "Ошибка! Введите корректное значение роста")
+			sendMessage(msg)
+			return
+		}
+		//calculateBMI
+		bmi.Height = height
+		bmiValue := bmi.CalcBMI()
+
+		msg := tgbotapi.NewMessage(chatId, "Ваш ИМТ: "+strconv.FormatFloat(bmiValue, 'f', 2, 64))
+		bot.Send(msg)
+
+		userStates[chatId] = InitialState
+
+	}
+
+	//TODO: more info about bmi
 }
--- pkg/bodycalc/bodycalc.go
+++ pkg/bodycalc/bodycalc.go
@@ -1,28 +1,22 @@
 package bodycalc
 
-import "errors"
+type UserState int
 
 type BMI struct {
-	Height float32 //cm
-	Weight float32 //kg
+	Height float64 //cm
+	Weight float64 //kg
 }
 
-func NewBMI(h float32, w float32) *BMI {
+func NewBMI(h float64, w float64) *BMI {
 	return &BMI{h, w}
 }
 
-func (bmi *BMI) CalcBMI() (float32, error) {
-
-	/*TODO: if User input symbols?*/
-
+func (bmi *BMI) CalcBMI() float64 {
 	if bmi.Weight == 0.0 || bmi.Height == 0.0 {
-		return 0.0, errors.New("Нет данных о весе или росте")
-
-	} else if bmi.Weight < 0.0 || bmi.Height < 0.0 || bmi.Weight > 500.0 || bmi.Height > 250.0 {
-		return 0.0, errors.New("Данные о росте или весе некорректны")
-	} else {
-		h := bmi.Height / 100 //meters
-		bmiValue := bmi.Weight / (h * h)
-		return bmiValue, nil
+		return 0.0
 	}
+	h := bmi.Height / 100 //meters
+	bmiValue := bmi.Weight / (h * h)
+	return bmiValue
+
 }
