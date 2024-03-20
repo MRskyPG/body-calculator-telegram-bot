0da8458054e91176e746b573a2cb48dad597f5e0
MRskyPG <mrogalsky8113@gmail.com>
2024-03-20 16:32:33 +0700

    rename directory

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"body-calculator-tg-bot/int/functions"
+	"body-calculator-tg-bot/internal/functions"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 	"log"
